a9a077d621bed3c59a091d23fce3ff6123f1cad6
ondrejvosmera <ondravosmera@seznam.cz>
2022-11-05 20:48:20 +0100

    state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react'
 import './App.css'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      markdown: "",
+    };
+  }
+
+  updateMarkdown(markdown) {
+    this.setState({ markdown });
+  }
   
   render() {
     return (
