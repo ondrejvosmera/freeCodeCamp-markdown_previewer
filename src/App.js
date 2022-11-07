8fe347facc4006bb35f4fc4741cb199ec1f6d53a
ondrejvosmera <ondravosmera@seznam.cz>
2022-11-07 17:05:17 +0100

    css+media_queries

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './App.css'
 import { marked } from 'marked'
+import { BsArrowsFullscreen } from 'react-icons/bs'
 
 export default class App extends Component {
   constructor(props) {
@@ -14,24 +15,34 @@ export default class App extends Component {
     this.setState({ markdown })
   }
 
-  
   render() {
     return (
       <div className='App'>
         <h1 className='title'>Markdown Previewer</h1>
         <div className='container'>
           <div className='editor'>
-            <h2 className='editor-title'>Editor</h2>
+            <div className='editor-title-container'>
+              <h2 className='editor-title'>Editor</h2>
+              <div className='fullscreen'>
+                <BsArrowsFullscreen />
+              </div>
+            </div>
             <div className='editor-container'>
               <textarea 
                 className='input' 
+                rows='20'
                 value={this.state.markdown} 
                 onChange={(e) => {this.updateMarkdown(e.target.value);}}>
               </textarea>
             </div>
           </div>
           <div className='previewer'>
-            <h2 className='previewer-title'>Previewer</h2>
+          <div className='previewer-title-container'>
+              <h2 className='previewer-title'>Previewer</h2>
+              <div className='fullscreen'>
+                <BsArrowsFullscreen />
+              </div>
+            </div>
             <div className='previewer-container' dangerouslySetInnerHTML={{ 
               __html: marked(this.state.markdown),
               }}>
